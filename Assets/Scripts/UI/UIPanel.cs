5cdb1cc8ae0b1f9d97397f6771bd12ddab86f764
sigfig <sigfig@translunar.space>
2015-09-27 07:36:24 -0500

    im a pro ux designer 100%

--- Assets/Scripts/UI/UIPanel.cs
+++ Assets/Scripts/UI/UIPanel.cs
@@ -2,4 +2,39 @@
 using System.Collections;
 
 public class UIPanel : MonoBehaviour {
+    public GameObject to;
+
+    public GameObject camera;
+
+    Vector3 fromPosition;
+    Quaternion fromRotation;
+    Vector3 fromScale;
+
+    float progress = 0;
+    public float transitionTime = 1;
+
+    void Start() {
+        fromPosition = transform.position;
+        fromRotation = transform.rotation;
+        fromScale = transform.localScale;
+    }
+
+    void Update() {
+        RaycastHit hitinfo = new RaycastHit();
+        Ray ray = new Ray(camera.transform.position, camera.transform.TransformVector(Vector3.forward));
+        if(Physics.Raycast(ray, out hitinfo)) {
+            if(hitinfo.collider.gameObject == this.gameObject){
+                progress += Time.deltaTime;
+            }
+        } else {
+            progress -= Time.deltaTime;
+        }
+
+        progress = Mathf.Clamp(progress, 0, 0.5f);
+
+        transform.position = Vector3.Lerp(fromPosition, to.transform.position, progress);
+        transform.localScale = Vector3.Lerp(fromScale, to.transform.localScale, progress);
+        transform.rotation = Quaternion.Lerp(fromRotation, to.transform.rotation, progress);
+
+    }
 }
