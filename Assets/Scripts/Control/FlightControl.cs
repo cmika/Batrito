04eb7aa109da72238a3198a822926d0dacc6fe1d
thechris <chrismika93@gmail.com>
2015-09-26 23:16:13 -0500

    assending/decending works pretty well

--- Assets/Scripts/Control/FlightControl.cs
+++ Assets/Scripts/Control/FlightControl.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class FlightControl : MonoBehaviour {
 	public Vector3 forceOffset;
 	public Rigidbody drone;
+	public float throttleForce;
+	public float maxTiltAngle;
 
 	// Use this for initialization
 	void Start () {
@@ -12,19 +14,30 @@ public class FlightControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		// Gravity
-		Vector3 gravity = new Vector3(0, Physics.gravity.y, 0);
+		// Get Left Stick input in World Space
+		/*Vector3 inputVector = new Vector3 (Input.GetAxis ("Left/Right"),
+		                                  0,
+		                                  Input.GetAxis ("Front/Back"));
+		inputVector = Quaternion.AngleAxis (transform.eulerAngles.y, Vector3.up) * inputVector;
 
+		float magnitude = inputVector.magnitude;
+
+
+
+		Vector3 desiredUpVector;
+		*/
 		// Resist Gravity
 		Vector3 force = new Vector3 (0, -Physics.gravity.y, 0);
 
 		// Adust for throttle
+		float throttle = Input.GetAxis ("Throttle") * throttleForce;
+		force = force + new Vector3 (0, throttle, 0);
 
-
-
-		// Apply forces 
-		drone.AddForceAtPosition (force, forceOffset);
-
-		print (drone.velocity);
+		// Gravity
+		Vector3 gravity = new Vector3(0, Physics.gravity.y, 0);
+		
+		// Apply forces
+		force = transform.TransformVector (force) + gravity;
+		drone.AddForceAtPosition (force * drone.mass, forceOffset);
 	}
 }
