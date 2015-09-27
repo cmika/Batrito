0a76a232bd32881722b28a9b402708307ff97f31
sigfig <sigfig@translunar.space>
2015-09-27 04:50:07 -0500

    commits don't matter at 5am

--- Assets/Scripts/UI/DroneAttitude.cs
+++ Assets/Scripts/UI/DroneAttitude.cs
@@ -2,4 +2,14 @@
 using System.Collections;
 
 public class DroneAttitude : MonoBehaviour {
+    public GameObject pitchControl;
+    public GameObject yawControl;
+    public GameObject rollControl;
+    public Transform target;
+
+    void Update() {
+        pitchControl.transform.localRotation = Quaternion.AngleAxis(target.eulerAngles.x, Vector3.forward);
+        yawControl.transform.localRotation = Quaternion.AngleAxis(target.eulerAngles.y, Vector3.forward);
+        rollControl.transform.localRotation = Quaternion.AngleAxis(target.eulerAngles.z, Vector3.forward);
+    }
 }
