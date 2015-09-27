ea200991a06d8893cf1ae31551aaf9520c2a7bb1
sigfig <sigfig@translunar.space>
2015-09-27 08:11:46 -0500

    x marks the spot

---
+++ Assets/Scripts/UI/MapMarker.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapMarker : MonoBehaviour {
+
+    public GameObject target;
+
+    void Update() {
+        //400 - 700 x
+        //0 - 300 z
+        var x = (300 - (target.transform.position.x - 400)) / 300;
+        var y = (300 - target.transform.position.z) / 300;
+        x = x * (3.8f * 2) - 3.8f;
+        y = y * (3.8f * 2) - 3.8f;
+
+        this.transform.localPosition = new Vector3(x, y, 2);
+    }
+}
