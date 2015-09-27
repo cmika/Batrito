f1b66694ce6ce59040fb5ef200cc6aac72825072
sigfig <sigfig@translunar.space>
2015-09-26 23:33:50 -0500

    pushing main for debug

---
+++ Assets/Scripts/Control/RecenterCamera.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.VR;
+using System.Collections;
+
+public class RecenterCamera : MonoBehaviour {
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            InputTracking.Recenter();
+        }
+    }
+}
